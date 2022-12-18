a88bb06b5f282a0d42eabfc0300699f38b4a10e3
behrooz shahabedin <admin@shopdochealth.com>
2022-12-18 14:08:50 +0400

    feat: first commit

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <section className='w-10/12 grid grid-cols-2 gap-x-6'>
+        <div className='border-2 rounded-lg border-gray-600 px-4 py-3 bg-white'>
+            <h2 className='text-lg font-semibold text-gray-600'>User</h2>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home
